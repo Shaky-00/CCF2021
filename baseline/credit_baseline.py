0ea022a9308565724bcb7b950e5f6048c85e6130
Shaky-00 <1306088206@qq.com>
2021-10-02 13:52:42 +0800

    update baseline

--- baseline/credit_baseline.py
+++ baseline/credit_baseline.py
@@ -10,7 +10,7 @@ warnings.filterwarnings('ignore')
 
 train_bank = pd.read_csv('./train_public.csv')
 train_internet = pd.read_csv('./train_internet.csv')
-train_bank
+train_bank = train_bank.rename(columns={'isDefault': 'is_default'})
 
 
 # In[2]:
@@ -175,143 +175,28 @@ test_data['industry'] = test_data['industry'].map(industry_dict)
 import lightgbm
 from sklearn import metrics
 
-X_train1 = train1_data.drop(['is_default','earlies_credit_mon','loan_id','user_id'], axis = 1, inplace = False)
+X_train1 = train1_data.drop(['is_default','earlies_credit_mon','loan_id','user_id','region'], axis = 1, inplace = False)
 y_train1 = train1_data['is_default']
 
-X_train2 = train2_data.drop(['is_default','earlies_credit_mon','loan_id','user_id'], axis = 1, inplace = False)
+X_train2 = train2_data.drop(['is_default','earlies_credit_mon','loan_id','user_id','region'], axis = 1, inplace = False)
 y_train2 = train2_data['is_default']
 
 X_train = pd.concat([X_train1, X_train2])
 y_train = pd.concat([y_train1, y_train2])
 
-X_test = test_data.drop(['earlies_credit_mon','loan_id','user_id'], axis = 1, inplace = False)
+X_test = test_data.drop(['earlies_credit_mon','loan_id','user_id','region'], axis = 1, inplace = False)
 
 # 利用Internet数据预训练模型1
-clf_ex=lightgbm.LGBMRegressor(n_estimators = 200)
+clf_ex=lightgbm.LGBMClassifier(n_estimators = 200)
 clf_ex.fit(X = X_train, y = y_train)
-clf_ex.booster_.save_model('LGBMmode.txt')
-pred = clf_ex.predict(X_test)
+clf_ex.booster_.save_model('LGBMLGBMClassifier.txt')
+pred = clf_ex.predict_proba(X_test)
+pred = pred[:,1]
 
 
 # In[17]:
 
 
 # submission
-submission = pd.DataFrame({'id':test['loan_id'], 'is_default':pred})
+submission = pd.DataFrame({'id':test['loan_id'], 'isDefault':pred})
 submission.to_csv('submission.csv', index = None)
-
-
-# #### NN
-
-# In[18]:
-
-
-import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-from keras.models import Sequential
-from keras.layers import Dense , Dropout , Lambda, Flatten
-from tensorflow.keras.optimizers import Adam, RMSprop
-from sklearn.model_selection import train_test_split
-from keras import  backend as K
-from keras import models
-from keras.preprocessing.image import ImageDataGenerator
-
-
-# In[19]:
-
-
-# 数据标准化
-import numpy as np
-mean_px = X_train.mean().astype(np.float32)
-std_px = X_train.std().astype(np.float32)
-def standardize(x): 
-    return (x-mean_px)/std_px
-
-
-# In[20]:
-
-
-# 缺失值填补
-X_train.fillna(0, inplace = True)
-X_test.fillna(0, inplace = True)
-
-
-# In[22]:
-
-
-X_train_NN =(X_train - mean_px) / std_px
-X_test_NN  = (X_test - mean_px) / std_px
-
-X_train_NN = (X_train.values).astype('float32') # all pixel values
-y_train_NN = y_train.astype('int32')
-
-X_test_NN = (X_test.values).astype('float32') # all pixel values
-
-
-# In[23]:
-
-
-# 修改初始化、加归一层、加dropout、改用不同的metrics
-seed = 43
-np.random.seed(seed)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.metrics import roc_auc_score
-import tensorflow as tf
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras.metrics import AUC
-
-def auroc(y_true, y_pred):
-    return tf.compat.v1.py_func(roc_auc_score, (y_true, y_pred), tf.double)
-
-input_shape = X_train_NN.shape[1]
-b_size = 1024
-max_epochs = 10
-
-import tensorflow.keras as K
-init = K.initializers.glorot_uniform(seed=1)
-simple_adam = K.optimizers.Adam(lr=0.001)
-
-model = K.models.Sequential()
-model.add(K.layers.Dense(units=256, input_dim=input_shape, kernel_initializer='he_normal', activation='relu',kernel_regularizer=l2(0.0001)))
-model.add(K.layers.LayerNormalization())
-model.add(K.layers.Dropout(0.3))
-model.add(K.layers.Dense(units= 64, kernel_initializer='he_normal', activation='relu'))
-model.add(K.layers.LayerNormalization())
-model.add(K.layers.Dropout(0.3))
-model.add(K.layers.Dense(units=1, kernel_initializer='he_normal', activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer=simple_adam, metrics=['accuracy',AUC(name='auc')])
-
-
-# In[24]:
-
-
-model.summary()
-
-
-# In[25]:
-
-
-print("Starting NN training")
-h = model.fit(X_train_NN, y_train_NN, batch_size=b_size, epochs=max_epochs, shuffle=True, verbose=1)
-print("NN training finished")
-
-
-# In[30]:
-
-
-pred_NN = model.predict(X_test_NN)
-pred_NN = [item[0] for item in pred_NN]
-
-
-# In[32]:
-
-
-model.save('NN_model.h5')
-submission = pd.DataFrame({'id':test['loan_id'], 'is_default':pred_NN})
-submission.to_csv('submission.csv', index = None)
-
-
-# # 其他尝试...
